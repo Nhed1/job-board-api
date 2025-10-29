8f7e5844d3a11be955886a5a94b67993cbb2e744
leonardo <leonardogarreto@gmail.com>
2025-10-29 19:44:48 -0300

    feat: init

---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { SupabaseService } from './supabase/supabase.service';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly supabaseService: SupabaseService) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async getSupabaseData() {
+    return { message: 'Supabase is connected and ready to use!' };
+  }
+}
